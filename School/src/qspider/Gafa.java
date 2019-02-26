248a2a3ecc44431f2632a70fd8db469864fa8357
nsneeraj <46974049+nsneeraj@users.noreply.github.com>
2019-02-26 22:25:00 +0530

    jhhh

---
+++ School/src/qspider/Gafa.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package qspider;
+
+/**
+ * @author Hp_Lappy
+ *
+ */
+public class Gafa {
+
+	/**
+	 * 
+	 */
+	public Gafa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
